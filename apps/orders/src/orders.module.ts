6875138d43f83d5505666e776c8a332e71634df9
ahmedsolimanpts <ahmedmsoly98.com>
2024-04-22 20:17:55 +0200

    Add env File To Orders App

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: joi.object({
+        MONGO_CLUSTER: joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
     MongooseModule.forRootAsync({
-      useFactory: () => ({ uri: 'mongodb://127.0.0.1:27017' }),
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_CLUSTER'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [OrdersController],
